67849c7689e1802bba099b25b97142cb5f4a55c8
Pavan Mhaisne <129740443+Stellar2504@users.noreply.github.com>
2024-09-27 11:46:42 +0000

    update in conversion.....

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -17,3 +17,8 @@ console.log(booleanIsLoggedIn)
 // 1 => true ; 0 => false
 // "" => false
 // "pavan" => true
+
+let someNumber = 33
+
+let valueInString = String(someNumber)
+console.log(typeof(valueInString))
