2e6bf2ff6587900309c0c11d77e5e7ac9165459e
Pavan Mhaisne <129740443+Stellar2504@users.noreply.github.com>
2024-09-27 18:14:19 +0000

    adding datatype summery....

---
+++ 01_basics/dataTypes_Summery.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 Types : Number , String , Boolean , null , undefined , BigInt , Symbol ;
+
+const score = 100
+const scoreValue = 1000.33
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail ;
+
+const id = Symbol('1223')
+const anotherId = Symbol('1223')
+
+console.log(id ===anotherId);
+
+const bigNumber = 123243575734534265473n
+
+
+
+// Non Primitive or Reference
+
+// Array , Objects , Functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+
+let myobj = {
+    name:"pavan",
+    age:"20"
+}
+
+function myFunction(){
+console.log("This is a function");
+
+}
